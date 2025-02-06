6057c80d1cdf576fec85293f4b669500eeebed89
Francis Badasu <nytro04@gmail.com>
2025-02-06 06:36:38 +0100

    updates: add DB connection

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -1,12 +1,17 @@
 package main
 
 import (
+	"context"
+	"database/sql"
 	"flag"
 	"fmt"
 	"log"
 	"net/http"
 	"os"
 	"time"
+
+	"github.com/joho/godotenv"
+	_ "github.com/lib/pq"
 )
 
 const version = "1.0.0"
@@ -14,6 +19,12 @@ const version = "1.0.0"
 type config struct {
 	port int
 	env  string
+	db   struct {
+		dsn          string // data source name
+		maxOpenConns int
+		maxIdleConns int
+		maxIdleTime  string
+	}
 }
 
 type application struct {
@@ -24,11 +35,36 @@ type application struct {
 func main() {
 	var cfg config
 
+	logger := log.New(os.Stdout, "", log.Ldate|log.Ltime)
+
 	flag.IntVar(&cfg.port, "port", 4000, "API server port")
 	flag.StringVar(&cfg.env, "env", "development", "Environment (development|staging|production)")
+	// DSL for database connection using config
+
+	err := godotenv.Load()
+	if err != nil {
+		logger.Fatal("Error loading .env file")
+	}
+
+	var (
+		dbHost = os.Getenv("DB_HOST")
+		// dbPort     = os.Getenv("DB_PORT")
+		dbUser     = os.Getenv("DB_USER")
+		dbPassword = os.Getenv("DB_PASSWORD")
+		dbName     = os.Getenv("DB_NAME")
+	)
+
 	flag.Parse()
 
-	logger := log.New(os.Stdout, "", log.Ldate|log.Ltime)
+	dsn := fmt.Sprintf("postgres://%s:%s@%s/%s?sslmode=disable", dbUser, dbPassword, dbHost, dbName)
+
+	db, err := openDB(cfg)
+	if err != nil {
+		logger.Fatal(err)
+	}
+
+	defer db.Close()
+	logger.Println("database connection pool established")
 
 	app := &application{
 		config: cfg,
@@ -45,6 +81,27 @@ func main() {
 
 	// Start the server
 	logger.Printf("starting %s server on %s", cfg.env, srv.Addr)
-	err := srv.ListenAndServe()
+	err = srv.ListenAndServe()
 	logger.Fatal(err)
+}
+
+func openDB(cfg config) (*sql.DB, error) {
+	// Open a sql.DB connection pool
+	db, err := sql.Open("postgres", cfg.db.dsn)
+	if err != nil {
+		return nil, err
+	}
+
+	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+	defer cancel()
+
+	// Ping the database with context above to check if the connection is working,
+	//if the couldnt be established after 5 seconds, this will return an error
+	err = db.PingContext(ctx)
+	if err != nil {
+		return nil, err
+	}
+
+	// return the sql.DB connection pool
+	return db, nil
 }
