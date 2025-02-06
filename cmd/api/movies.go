ce8926a0483d8f754a0832870761512200543ce3
Francis Badasu <nytro04@gmail.com>
2025-02-06 06:14:38 +0100

    updates: add movie validation and validation error response

--- cmd/api/movies.go
+++ cmd/api/movies.go
@@ -6,6 +6,7 @@ import (
 	"time"
 
 	"github.com/nytro04/greenlight/internal/data"
+	"github.com/nytro04/greenlight/internal/validator"
 )
 
 func (app *application) createMovieHandler(w http.ResponseWriter, r *http.Request) {
@@ -19,6 +20,21 @@ func (app *application) createMovieHandler(w http.ResponseWriter, r *http.Reques
 	err := app.readJSON(w, r, &input)
 	if err != nil {
 		app.badRequestResponse(w, r, err)
+		return
+	}
+
+	movie := data.Movie{
+		Title:   input.Title,
+		Runtime: input.Runtime,
+		Genres:  input.Genres,
+		Year:    input.Year,
+	}
+
+	// Initialize a new Validator instance.
+	v := validator.New()
+
+	if data.ValidateMovie(v, movie); !v.Valid() {
+		app.failedValidationResponse(w, r, v.Errors)
 		return
 	}
 
